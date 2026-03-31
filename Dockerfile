00f8b384c5538c6057d1a3059bf14f8b80da2667
Alexander De Sousa <60302715+alexdesousa7@users.noreply.github.com>
2026-03-31 15:06:47 +0200

    Correcion de codigo en varios archivos

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,6 @@ COPY app ./app
 
 ENV PYTHONPATH=/app
 
-EXPOSA 8080
+EXPOSE 8080
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
-from . import models, sschemas
-from faastapi import HTTPException, status
+from . import models, schemas
+from fastapi import HTTPException, status
 import logging
 
 logger = logging.getLogger("students-crud")
@@ -20,7 +20,7 @@ def get_student_by_id(db: Session, student_id: int):
 def create_student(db: Session, student_in: schemas.StudentCreate):
     logger.info(f"Creando el email={student_in.email} para el estudiante")
     # Verificar si el email del estudiante ya existe
-    existing = db.query(models.Student).filter(models.Student.email == student_i.email).first()
+    existing = db.query(models.Student).filter(models.Student.email == student_in.email).first()
     if existing:
         logger.error(f"El email {student_in.email} ya existe")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="email already exists")
